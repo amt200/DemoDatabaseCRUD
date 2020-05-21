9d7499f96760e6c24fb207b14390bc7715d3e42c
18045945 <aungmoethet2000@gmail.com>
2020-05-21 12:35:35 +0800

    First commit

--- app/src/main/java/com/myapplicationdev/android/demodatabasecrud/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demodatabasecrud/MainActivity.java
@@ -1,5 +1,6 @@
 package com.myapplicationdev.android.demodatabasecrud;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //initialize the variables with UI here
+        tvDBContent = findViewById(R.id.tvDBContent);
+        etContent = findViewById(R.id.etContent);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnEdit = findViewById(R.id.btnEdit);
+        btnRetrieve = findViewById(R.id.btnRetrieve);
 
         al = new ArrayList<Note>();
 
@@ -45,6 +51,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Note target = al.get(0);
+
+                Intent i = new Intent(MainActivity.this,
+                        EditActivity.class);
+                i.putExtra("data", target);
+                startActivityForResult(i, 9);
+            }
+        });
+
+
         btnRetrieve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,4 +83,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == 9){
+            btnRetrieve.performClick();
+        }
+    }
+
 }
